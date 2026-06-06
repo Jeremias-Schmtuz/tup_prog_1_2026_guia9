fc13e5545b9e593ed66b3690cb2451535550db36
Jeremias-Schmutz <schmutzjeremias@gmail.com>
2026-06-05 23:16:24 -0600

    Guia 9 en proceso

---
+++ Guia 9/Ejercicio 7/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_7
+{
+    internal class Program
+    {
+
+        static Servicio servicio = new Servicio();
+
+        #region SOLICITAR OPCION MENU
+        static int MostrarPantallaSolicitarOpcionMenu()
+        {
+            Console.WriteLine("1-Ingresar un resumen de venta");
+            Console.WriteLine("2-Mostrar numero de transaccion con mayor monto");
+            Console.WriteLine("3-Mostrar porcentaje de cantidad por rubro");
+            Console.WriteLine("4-Mostrar la recaudacion total");
+            Console.WriteLine("5-Salir");
+            int opcion = Convert.ToInt32(Console.ReadLine());
+            return opcion;
+        }
+        #endregion
+
+        #region REGISTRAR TRANSACCION
+        static void MostrarPantallaRegistrarTransaccion()
+        {
+            Console.WriteLine("Ingrese el numero de transaccion");
+            int nroTransaccion = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el rubro (1-5)");
+            int rubro = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese la cantidad de productos vendidos");
+            int cantidad = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el monto total");
+            double monto = Convert.ToDouble(Console.ReadLine());
+            servicio.EvaluarTransaccionPuntoDeVenta(nroTransaccion, rubro, cantidad, monto);
+        }
+        #endregion
+
+        #region MOSTRAR PORCENTAJE DE CANTIDADES POR RUBRO
+        static void MostrarPantallaPorcentajeDeCantidadesPorRubro()
+        {
+            servicio.CalcularPorcentajesCantidadVentasPorRubro();
+            Console.WriteLine($"Rubro 1: {servicio.porcentajeCantidadRubro[0]} %");
+            Console.WriteLine($"Rubro 2: {servicio.porcentajeCantidadRubro[1]} %");
+            Console.WriteLine($"Rubro 3: {servicio.porcentajeCantidadRubro[2]} %");
+            Console.WriteLine($"Rubro 4: {servicio.porcentajeCantidadRubro[3]} %");
+            Console.WriteLine($"Rubro 5: {servicio.porcentajeCantidadRubro[4]} %");
+        }
+        #endregion
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Guia 9/Ejercicio 7/Servicio.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_7
+{
+    internal class Servicio
+    {
+
+        #region Variables "GLOBALES"
+        private int[] cantidades = new int[5];
+        public int numeroTransaccionMayor;
+        public double montoTransaccionMayor;
+        private int contadorDeTransacciones;
+        public double[] porcentajeCantidadRubro = new double[5];
+        public double recaudacionTotal;
+        #endregion
+
+        #region INICIALIZAR VARIABLES
+        public void InicializarVariables()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                cantidades[i] = 0;
+                porcentajeCantidadRubro[i] = 0;
+            }
+            numeroTransaccionMayor = 0;
+            contadorDeTransacciones = 0;
+            montoTransaccionMayor = 0;
+            recaudacionTotal = 0;
+        }
+        #endregion
+
+        #region EVALUAR TRANSACCION PUNTO DE VENTA
+        public void EvaluarTransaccionPuntoDeVenta(int nroTransaccion, int rubro, int cantidad, double monto)
+        {
+            cantidades[rubro - 1] += cantidad;
+
+            if (monto > montoTransaccionMayor)
+            {
+                montoTransaccionMayor = monto;
+                numeroTransaccionMayor = nroTransaccion;
+            }
+
+            recaudacionTotal += monto;
+        }
+        #endregion
+
+        #region CALCULAR PORCENTAJES Y CANTIDADES DE VENTAS POR RUBRO
+        public void CalcularPorcentajesCantidadVentasPorRubro()
+        {
+            int totalCantidad = cantidades[0] + cantidades[1] + cantidades[2] + cantidades[3] + cantidades[4];
+            for (int i =0; i < 5; i++)
+            {
+                porcentajeCantidadRubro[i] = (double)cantidades[i] / totalCantidad;
+            }
+        }
+        #endregion
+    }
+}
