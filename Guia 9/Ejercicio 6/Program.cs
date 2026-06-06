62ab0292371de5b668a294c750e5d61429f7e0ef
Jeremias-Schmutz <schmutzjeremias@gmail.com>
2026-06-05 21:43:54 -0600

    Guia 9 en proceso

---
+++ Guia 9/Ejercicio 6/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ejercicio_6
+{
+    internal class Program
+    {
+
+        static Servicio servicio = new Servicio();
+
+        #region SOLICITAR OPCIN MENU
+        static int MostrarPantallaSolicitarOpcionMenu()
+        {
+            Console.WriteLine("1-Ingresar opinion");
+            Console.WriteLine("2-Procesar y mostrar resultados encuesta");
+            Console.WriteLine("3-Salir");
+            int opcion = Convert.ToInt32(Console.ReadLine());
+            return opcion;
+        }
+        #endregion
+
+        #region REGISTRAR ENCUESTA
+        static void MostrarPantallaRegistrarEncuesta()
+        {
+            Console.WriteLine("0-Positivo");
+            Console.WriteLine("1-Negativo");
+            Console.WriteLine("2-Indeciso");
+            Console.Write("Ingrese su opinion");
+            int opinion = Convert.ToInt32(Console.ReadLine());
+            servicio.RegistrarOpcion(opinion);
+        }
+        #endregion
+
+        #region PROCESAR Y MOSTRAR RESULTADOS ENCUESTA
+        static void MostrarPantallaProcesarMostrarResultadosEncuesta()
+        {
+            servicio.ProcesarEncuesta();
+            Console.WriteLine("Positivos: " + servicio.porcentajePositivos + "%");
+            Console.WriteLine("Negativos: " + servicio.porcentajeNegativos + "%");
+            Console.WriteLine("Indecisos: " + servicio.porcentajeIndecisos + "%");
+        }
+        #endregion
+
+        static void Main(string[] args)
+        {
+            int opcion;
+            do
+            {
+                opcion = MostrarPantallaSolicitarOpcionMenu();
+                switch (opcion)
+                {
+                    case 1:
+                        {
+                            MostrarPantallaRegistrarEncuesta(); break;
+                        }
+                    case 2:
+                        {
+                            MostrarPantallaProcesarMostrarResultadosEncuesta(); break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("Saliendo...");
+                            Thread.Sleep(2500); break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Opcion invalida.");
+                            Console.WriteLine("Precione cualquier tecla volver al menu de opciones");
+                            Console.ReadKey(); break;
+                        }
+                }
+            }
+            while (opcion != 3);
+
+        }
+    }
+}
---
+++ Guia 9/Ejercicio 6/Servicio.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_6
+{
+    internal class Servicio
+    {
+
+        #region Variables "GLOBALES"
+        private int indecisos;
+        private int negativos;
+        private int positivos;
+        public double porcentajeIndecisos;
+        public double porcentajeNegativos;
+        public double porcentajePositivos;
+        #endregion
+
+        #region REGISTRAR OPCION
+        public void RegistrarOpcion(int opcion)
+        {
+            if (opcion == 0)
+            {
+                positivos++;
+            }
+            else if (opcion == 1)
+            {
+                negativos++;
+            }
+            else if (opcion == 2)
+            {
+                indecisos++;
+            }
+        }
+        #endregion
+
+        #region PROCESAR ENCUESTA
+        public void ProcesarEncuesta()
+        {
+            int total = positivos + negativos + indecisos;
+            porcentajePositivos = ((double)positivos / total) * 100;
+            porcentajeNegativos = ((double)negativos / total) * 100;
+            porcentajeIndecisos = ((double)positivos / total) * 100;
+        }
+        #endregion
+
+    }
+}
